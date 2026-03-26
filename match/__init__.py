b4b832b09abff571ca0bb6781a0205a4c42d9f5c
Max <max@privatevoid.net>
2026-03-27 00:58:27 +0100

    match: filter empty rules in any and all

--- match/__init__.py
+++ match/__init__.py
@@ -1,9 +1,14 @@
 def all(*items):
+    items = list(filterEmptyRules(items))
     return {
         "all": items
-    }
+    } if len(items) != 1 else items[0]
 
 def any(*items):
+    items = list(filterEmptyRules(items))
     return {
         "any": items
-    }
+    } if len(items) != 1 else items[0]
+
+def filterEmptyRules(rules):
+    return filter(lambda x: x != {}, rules)
